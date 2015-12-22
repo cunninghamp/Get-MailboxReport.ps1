18474407912211a9e90fe8bf79c558a66eac7408
Paul Cunningham <paul@locklan.com.au>
2015-12-22 17:50:35 +1000

    Update Get-MailboxReport.ps1

--- Get-MailboxReport.ps1
+++ Get-MailboxReport.ps1
@@ -75,7 +75,31 @@ Additional Credits:
 Chris Brown, http://www.flamingkeys.com
 Boe Prox, http://learn-powershell.net/
 
-Change Log
+License:
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Paul Cunningham
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Change Log:
 V1.00, 2/2/2012 - Initial version
 V1.01, 27/2/2012 - Improved recipient scope settings, exception handling, and custom file name parameter.
 V1.02, 16/10/2012 - Reordered report fields, added OU, primary SMTP, some specific folder stats,
